afaf1be961ce3541daac2630003c3c703a91e5b9
newbullet23 <bullet23@mail.ru>
2020-10-16 12:57:49 +0300

    release v1

---
+++ MainClass.java
@@ -0,0 +1,30 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.gecko.driver", "C:\\Users\\admin\\IdeaProjects\\Test123Project\\drivers\\geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://mail.ru/");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+
+        driver.findElement(By.xpath("//input[@id=\"mailbox:login-input\"]")).sendKeys("testpost233");
+        driver.findElement(By.xpath("//input[@class=\"o-control\"]")).submit();
+        driver.manage().timeouts().pageLoadTimeout(5, TimeUnit.SECONDS);
+        driver.findElement(By.xpath("//input[@id=\"mailbox:password-input\"]")).sendKeys("Ntcnjdsqgfhjkm123");
+        WebElement button = driver.findElement(By.xpath("//input[@class=\"o-control\"]"));
+        button.submit();
+        driver.findElement(By.xpath("//a[contains(@class, 'compose-button compose-button_white')]")).click();
+        driver.findElement(By.xpath("//input[contains(@tabindex,'100')]")).sendKeys("idedov@at-consulting.ru");
+        driver.findElement(By.xpath("//input[contains(@name,'Subject')]")).sendKeys("Test task");
+        driver.findElement(By.xpath("//div[@role='textbox']")).sendKeys("http://google.com");
+        driver.findElement(By.xpath("//span[contains(@class,'button2_primary')]")).click();
+        driver.quit();
+    }
+}
